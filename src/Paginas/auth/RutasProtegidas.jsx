36d2411255f911f9cfb9d986de2ae7091183b076
Diego Rodriguez <mundie192@gmail.com>
2024-07-14 08:58:47 -0500

    cambios menores

--- src/Paginas/auth/RutasProtegidas.jsx
+++ src/Paginas/auth/RutasProtegidas.jsx
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import { Navigate } from 'react-router-dom';
-import swal from 'sweetalert';
+import Alert from '../../Extencion/Aviso';
 
 
 const RutasProtegidas = ({ element }) => {
